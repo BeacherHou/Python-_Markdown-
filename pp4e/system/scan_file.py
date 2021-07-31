e3fb65d154d7d952258067376833ba7f6c52ae8b
BeacherHou <houyuze.coder@qq.com>
2021-07-31 21:08:30 +0800

    创建pp4e/system/scan_file.py：添加注释

--- pp4e/system/scan_file.py
+++ pp4e/system/scan_file.py
@@ -4,12 +4,12 @@
 def file_scanner(fn_str, function):
 	"通用文件扫描例行函数"
 
-	file = open(fn_str, 'r')
+	file = open(fn_str, 'r')					# 创建文件对象
 
 	while True:
-		line_str = file.readline()
+		line_str = file.readline()				# 调用文件方法
 		if not line_str:
-			break
-		function(line_str)
+			break								# 直到文件末尾
+		function(line_str)						# 调用一个函数对象
 
 	file.close()

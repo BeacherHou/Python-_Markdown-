7e3de38ab74a9220b5fd0becb19f52f449f9f516
BeacherHou <houyuze.coder@qq.com>
2021-07-31 21:51:23 +0800

    修改pp4e/system/scan_file.py：添加file_scanner函数的四个替代方案，并将其注释

--- pp4e/system/scan_file.py
+++ pp4e/system/scan_file.py
@@ -13,3 +13,29 @@ def file_scanner(fn_str, function):
 		function(line_str)						# 调用一个函数对象
 
 	file.close()
+
+
+# 替代方案 A
+# def file_scanner(fn_str, function):
+# 	# 使用文件迭代器逐行扫描
+# 	for line_str in open(fn_str, 'r'):
+# 		# 调用一个函数对象
+# 		function(line_str)
+
+
+# 替代方案 B
+# def file_scanner(fn_str, function):
+# 	# 使用map代替了for循环
+# 	list(map(function, open(fn_str, 'r')))
+
+
+# 替代方案 C
+# def file_scanner(fn_str, function):
+# 	# 使用列表解析代替for循环
+# 	[function(line_str) for line_str in open(fn_str, 'r')]
+
+
+# 替代方案 D
+# def file_scanner(fn_str, function):
+# 	# 使用列表解析代替for循环
+# 	list(function(line_str) for line_str in open(fn_str, 'r'))

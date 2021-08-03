e4ecfb756d59c8ccaac3dd63aae371394c7f12e8
BeacherHou <houyuze.coder@qq.com>
2021-08-03 22:34:24 +0800

    修改 my_PP4E/system/fork_count.py：测试程序

--- my_PP4E/system/fork_count.py
+++ my_PP4E/system/fork_count.py
@@ -14,7 +14,7 @@ def counter(count_int):
 	"子进程"
 
 	for i_int in range(count_int + 1):
-		time.sleep(1)
+		time.sleep(0.25)
 		print('\n{pid_int} -> {i_int}'.format(pid_int=os.getpid(), i_int=i_int))
 
 
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 		if pid_int != 0:
 			print('Process {pid_int} spawned'.format(pid_int=pid_int))
 		else:
-			counter(5)
+			counter(60)
 			os._exit(0)
 
 	print('Main process exiting.')

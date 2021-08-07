5325d15d920f85fac4d30f019be72db483076c6d
BeacherHou <houyuze.coder@qq.com>
2021-08-07 14:01:32 +0800

    创建 my_PP4E/system/thread_count_wait_2.py：利用bool列表在父线程中探知子线程何时结束

---
+++ my_PP4E/system/thread_count_wait_2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"利用bool列表在父线程中探知子线程何时结束"
+
+import _thread as thread
+
+MUTEX = thread.allocate_lock()
+NUMS_THREAD_INT = 10
+LISTBOOL = [False for i_int in range(NUMS_THREAD_INT)]
+
+
+def counter(id_int, count_int):
+	"数数"
+	for i_int in range(count_int + 1):
+		MUTEX.acquire()
+		print('[{}] -> {}'.format(id_int, i_int))
+		MUTEX.release()
+	LISTBOOL[id_int] = True  # 向主线程发送信号
+
+
+def main():
+	for i_id_int in range(NUMS_THREAD_INT):
+		thread.start_new_thread(counter, (i_id_int, 100))
+	while False in LISTBOOL:
+		pass
+	print('Main thread exiting...')
+
+
+if __name__ == '__main__':
+	main()
